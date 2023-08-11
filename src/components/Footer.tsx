40d6347b21969cfb9f136a9328da81b40cc9f221
nescheytt <nahuel.scheytt@gmail.com>
2023-08-11 20:45:35 +0100

    feat: add switcher theme

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { motion as m } from 'framer-motion'
+import SwitcherTheme from './SwitcherTheme'
 
 const container = {
   hidden: { opacity: 0 },
@@ -19,16 +20,17 @@ const item = {
 
 export default function Footer() {
   return (
-    <m.footer variants={container} initial="hidden" animate="show" className="w-full flex flex-col justify-between">
+    <m.footer variants={container} initial="hidden" animate="show" className="w-full flex flex-col md:flex-row md:items-center justify-between">
+      <m.div variants={item} className="flex gap-10 items-center">
+        <p>© 2023 XHunterTech</p>
+        <SwitcherTheme />
+      </m.div>
+
       <m.ol variants={item} className="flex justify-end gap-10">
         <li>Home</li>
         <li>About</li>
         <li>Contact</li>
       </m.ol>
-
-      <m.div variants={item} className="flex justify-between gap-10">
-        <p>© 2023 XHunterTech</p>
-      </m.div>
     </m.footer>
   )
 }
