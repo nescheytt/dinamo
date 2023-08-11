6b370ca20ab1897ac21e405ac5ba5ca38eeddae1
Nahuel Scheytt <nahuel.scheytt@gmail.com>
2023-08-11 17:40:36 +0100

    [Feat] About component (#2)
    * feat: add about component
    
    * feat: new hero gradient
    
    * feat: imp box styles
    
    * feat: change wording

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,16 @@
 import Gradient from '@/components/Gradient'
 import Header from '@/components/Header'
 import Banner from '@/components/Banner'
+import About from '@/components/About'
 import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-between py-12 px-24 relative">
+    <main className="min-h-screen flex flex-col items-center justify-between py-12 px-5 md:px-24 relative bg-zinc-900">
       <Gradient />
       <Header />
       <Banner />
-
-      <section className="w-full min-h-screen flex flex-col items-center justify-center border-b border-gray-700 gap-20">
-        <div>
-          <h3 className="w-full text-3xl font-extrabold text-gray-300 text-center mb-3">
-            {`What's XHunter?`}
-          </h3>
-          <p className="text-xl text-center font-light text-gray-500">Consectetur aliqua culpa culpa tempor occaecat amet sunt amet eu aliquip enim reprehenderit.</p>
-        </div>
-
-        <div className="max-w-6xl flex gap-10">
-          <div className="w-full relative transition duration-1000 ring-2 ring-slate-500 ring-inset rounded-xl p-[1px] bg-animation">
-            <div className="h-full rounded-xl z-10 relative bg-white dark:bg-black p-4">
-              <h3 className="text-lg font-bold mb-2">Optimizations</h3>
-              <p className="text-md font-light text-gray-500">
-                Navigate your digital products at light speed. Scroll in peace.
-              </p>
-            </div>
-          </div>
-
-          <div className="w-full relative transition duration-1000 ring-2 ring-slate-500 ring-inset rounded-xl p-[1px] bg-animation">
-            <div className="h-full rounded-xl z-10 relative bg-white dark:bg-black p-4">
-              <h3 className="text-lg font-bold mb-2">Dynamic Design</h3>
-              <p className="text-md font-light text-gray-500">
-                Modern designs for your product.
-              </p>
-            </div>
-          </div>
-
-          <div className="w-full relative transition duration-1000 ring-2 ring-slate-500 ring-inset rounded-xl p-[1px] bg-animation">
-            <div className="h-full rounded-xl z-10 relative bg-white dark:bg-black p-4">
-              <h3 className="text-lg font-bold mb-2">Consultancy</h3>
-              <p className="text-md font-light text-gray-500">
-                We answer all your questions in record time.
-              </p>
-            </div>
-          </div>
-        </div>
-      </section>
+      <About />
 
       <section className="w-full min-h-screen flex items-center justify-center border-b border-gray-700 gap-20">
         <div className="max-w-lg">
