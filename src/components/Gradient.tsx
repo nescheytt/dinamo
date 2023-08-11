e4ada2d4563230f252be43d5c9452b65cea440af
nescheytt <nahuel.scheytt@gmail.com>
2023-08-11 17:31:05 +0100

    feat: new hero gradient

--- src/components/Gradient.tsx
+++ src/components/Gradient.tsx
@@ -1,15 +1,10 @@
+import Image from 'next/image'
+import heroGradient from '@/assets/images/hero-gradient.png'
+
 export default function Gradient() {
   return (
-    <div className="w-full min-h-screen bg-gradient-to-r from-indigo-500 opacity-50 absolute left-0 top-0">
-      <div
-        style={{ 
-          width: '100%',
-          height: '100%',
-          position: 'absolute',
-          backgroundSize: '57px 57px',
-          backgroundImage: 'linear-gradient(hsla(239, 84%, 67%, 0.09) .1em, transparent 0), linear-gradient(90deg, hsla(239, 84%, 67%, 0.09) .1em, transparent 0)'
-        }}
-      />
+    <div className="w-full min-h-screen absolute left-0 top-0">
+      <Image src={heroGradient} alt="hero gradient" className="w-full h-full opacity-30" />
     </div>
   )
 }
