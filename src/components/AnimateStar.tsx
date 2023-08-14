1ab15bf3a82b0955ff842bd00fb781fe03ce61a0
nescheytt <nahuel.scheytt@gmail.com>
2023-08-14 20:38:58 +0100

    feat: add impr animations

--- src/components/AnimateStar.tsx
+++ src/components/AnimateStar.tsx
@@ -4,7 +4,7 @@ import { motion as m } from 'framer-motion'
 
 export default function AnimateStar() {
   return (
-    <m.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} className="w-full relative">
+    <m.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: .75 }} className="w-full relative">
       <div className="animate-shooting-star" /> 
     </m.div>
   )
