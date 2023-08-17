020436c8e18523b90a154360fc83a5f392c4e0b0
nescheytt <nahuel.scheytt@gmail.com>
2023-08-17 11:01:35 +0100

    feat: add styles mobile

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,8 +4,8 @@ import { motion as m } from 'framer-motion'
 
 export default function Contact() {
   return (
-    <section className="min-h-screen flex flex-col items-center justify-center gap-10">
-      <m.h3 initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 1.50 } }} className="w-full text-6xl font-bold text-gray-300 text-center mb-3">
+    <section className="min-h-screen flex flex-col items-center justify-center gap-5 lg:gap-10">
+      <m.h3 initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 1.50 } }} className="w-full text-4xl lg:text-6xl font-bold text-gray-300 text-center">
         Start with XHunter, today.
       </m.h3>
 
@@ -14,7 +14,7 @@ export default function Contact() {
         animate={{ opacity: 1, transition: { duration: 1.50 } }}
         href='mailto:contact@xhunter.com'
         className="
-          text-2xl font-light relative
+          text-lg lg:text-2xl font-light relative
           before:absolute before:left-0 before:-bottom-1 before:h-[1px] before:w-full before:z-50 before:bg-zinc-200 before:origin-left before:scale-x-0 before:transition-transform hover:before:scale-x-100
           after:absolute after:left-0 after:-bottom-1 after:z-0 after:h-[1px] after:w-full after:bg-zinc-600"
         >
